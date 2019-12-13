e09665eee221c23b4c63a6d79cb8c45780357c8b
어지훈 <hilton3180@gmail.com>
2019-12-13 15:52:54 +0900

    prim_algorithm 설명추가

--- data_processing/data_processing/prim_algorithm.c
+++ data_processing/data_processing/prim_algorithm.c
@@ -47,7 +47,21 @@ void prim(int s, int n) {
 					dist[v] = weight[u][v];
 		}
 	}
+	for(i=0;i<n;i++)
+		printf("[%d]", dist[i]);
 }
 int main() {
 	prim(0, MAX_VERTICES);
 }
+//dist의 의미는 weight[u][v]에서 u가 증가할때마다 u행의 v열에서 가장 작은 연결된 인덱스값을 찾아 dist의 인덱스에 넣어주는 것이다.
+//해당 노드에 연결되어있는 최소 간선을 dist에 저장시켜주는것
+/*
+의미를 보자
+1) dist값을 INF로, select값을 false로 모두 init
+2) dist[s]가 0인 이유는 자기자신을 순회하는 경로는 없음을 의미하는것
+3) u=get_min_vertex(n)으로 해당경로에서 최솟값을 지정한다.
+4) 처음에는 dist[s]외에 전부 INF로 되어있기에 dist[s]는 시작노드를 명시해주는것을 의미하기도 함
+5) select[u]를 TRUE로 해주는 것으로 노드에서 가장 최소 가중값을 가진 엣지를 방문표시하여 중복방문을 방지
+6) weight[u][v]에서 u행의 v열들을 모두 접근하여 최솟값을 찾음(u노드에서 최소경로를 찾아 dist[v]에 저장)
+#dist[v]에 저장하는 이유는 같은 v노드를 가리키는 가중값중에서 더 작은 값을 가진 경로가 있을경우 바꿔주기 위함
+*/
